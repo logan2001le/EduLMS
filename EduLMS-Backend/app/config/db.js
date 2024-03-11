3d18a3f1c291835520f7ad668f135dbaed751dfc
Long Le Duy <106512216+logan2001le@users.noreply.github.com>
2024-03-11 11:14:17 +0700

    Edit , optimize code

--- EduLMS-Backend/app/config/db.js
+++ EduLMS-Backend/app/config/db.js
@@ -4,7 +4,7 @@ const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: 'root',
-    database: 'edutrack'
+    database: 'edutrack1'
 });
 
 module.exports = db;
--- EduLMS-Backend/app/controllers/dashboardController.js
+++ EduLMS-Backend/app/controllers/dashboardController.js
@@ -1,33 +1,25 @@
-const db = require('../config/db');
+const db = require("../config/db");
 
 const dashboardController = {
-    getStatistics: async (req, res) => {
-        try {
-            // Đếm số lượng người dùng
-            const [userCountResult] = await db.execute('SELECT COUNT(*) AS userCount FROM users');
-            const userCount = userCountResult[0].userCount;
+  getStatistics: async (req, res) => {
+    try {
+      // Đếm số lượng người dùng
+      const [userCountResult] = await db.execute(
+        "SELECT COUNT(*) AS userCount FROM users"
+      );
+      const userCount = userCountResult[0].userCount;
 
-        
-            // // Lấy tổng số lượng các bản ghi sự kiện từ lịch sử sự kiện
-            // const [eventHistoryCountResult] = await db.execute('SELECT COUNT(*) AS eventHistoryCount FROM asset_event_history');
-            // const eventHistoryCount = eventHistoryCountResult[0].eventHistoryCount;
+      // Tổng hợp dữ liệu và trả về cho client
+      const statistics = {
+        userCount,
+      };
 
-            // Tính tổng số lượng khách hàng
-            // const [customerCountResult] = await db.execute('SELECT COUNT(*) AS customerCount FROM customers');
-            // const customerCount = customerCountResult[0].customerCount;
-
-       
-            // Tổng hợp dữ liệu và trả về cho client
-            const statistics = {
-                userCount,
-            };
-
-            res.status(200).json(statistics);
-        } catch (error) {
-            console.error('Error fetching statistics:', error);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
-    },
+      res.status(200).json(statistics);
+    } catch (error) {
+      console.error("Error fetching statistics:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  },
 };
 
 module.exports = dashboardController;
